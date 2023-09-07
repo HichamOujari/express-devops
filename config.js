5b5869eb382bbf021818d7b3e02242bfa1223a82
hichamoujari <hicham.oujari@izi.net>
2023-09-07 10:42:02 +0100

    sdasd

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: process.env.DB_HOST,
-  db_name: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  db_name: process.env.DB_NAME || "test",
   db_username: 'root',
-  db_password: process.env.DB_PASSWORD,
+  db_password: process.env.DB_PASSWORD || "",
 };
