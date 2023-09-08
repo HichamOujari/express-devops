63d6fa7dfe9314bc8899a9364e4fa9f9511225ed
hichamoujari <hicham.oujari@izi.net>
2023-09-08 13:40:31 +0100

    sdasd

--- devops/delete.sh
+++ devops/delete.sh
@@ -1,8 +1,10 @@
+namespace="--namespace=$1"
+
 echo "-------------------------- Delete the configation ------------------------------"
-kubectl delete -f config.yaml
+kubectl delete -f config.yaml $namespace
 
 echo "---------------------------- Delete DataBase --------------------------------"
-kubectl delete -f db.yaml
+kubectl delete -f db.yaml $namespace
 
 echo "--------------------------- Delete Application ------------------------------"
-kubectl delete -f app.yaml
+kubectl delete -f app.yaml $namespace
