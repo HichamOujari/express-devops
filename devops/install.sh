a328515c4d5fe74f2b7447ebd8878d5e8b2ff613
hichamoujari <hicham.oujari@izi.net>
2023-09-08 15:08:29 +0100

    sdasd

--- devops/install.sh
+++ devops/install.sh
@@ -1,10 +1,10 @@
 namespace="--namespace=$1"
 
 echo "--------------------------- Build docker image ------------------------------"
-#docker build -t hichamouja99/ec:4.0 ../
+docker build -t hichamouja99/ec:4.0 ../
 
 echo "\n---------------------------- Push docker image ------------------------------"
-#docker push hichamouja99/ec:4.0
+docker push hichamouja99/ec:4.0
 
 echo "\n-------------------------- Set the configation ------------------------------"
 kubectl apply -f config.yaml $namespace
@@ -13,7 +13,10 @@ echo "\n---------------------------- Deploy DataBase ---------------------------
 kubectl apply -f db.yaml $namespace
 
 echo "\n------------------------ Waiting DataBase start ------------------------------"
-sleep 60
+sleep 10
 
 echo "\n--------------------------- Deploy Application ------------------------------"
 kubectl apply -f app.yaml $namespace
+
+echo "\n------------------------------ Open Tunnel ----------------------------------"
+minikube tunnel
