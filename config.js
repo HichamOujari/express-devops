69dae3bfd3fc222203a0834ae5562367b6610be3
hichamoujari <hicham.oujari@izi.net>
2023-09-10 22:54:38 +0100

    sdasd

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  host: process.env.DB_HOST || "localhost",
-  db_name: process.env.DB_NAME || "test",
-  db_username: 'root',
-  db_password: process.env.DB_PASSWORD || "",
-};
--- entities/user.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-
-module.exports = {
-  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: Sequelize.STRING },
-  email: { type: Sequelize.STRING },
-};
