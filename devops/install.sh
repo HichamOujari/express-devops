7fd5e52d587293dd4f1c8ea6ed754acd464f61f0
hichamoujari <hicham.oujari@izi.net>
2024-02-20 18:14:50 +0100

    sdasd

--- devops/install.sh
+++ devops/install.sh
@@ -1,10 +1,10 @@
 namespace="--namespace=$1"
 
 echo "--------------------------- Build docker image ------------------------------"
-docker build -t hichamouja99/ec:4.0 ../
+#docker build -t hichamouja99/ec:4.0 ../
 
 echo "\n---------------------------- Push docker image ------------------------------"
-docker push hichamouja99/ec:4.0
+#docker push hichamouja99/ec:4.0
 
 echo "\n-------------------------- Set the configation ------------------------------"
 kubectl apply -f config.yaml $namespace
